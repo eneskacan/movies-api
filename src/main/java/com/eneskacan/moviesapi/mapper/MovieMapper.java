cfe3d02243b49e17a13e10637b5eaeb20337388d
eneskacan <enes.kacan@agu.edu.tr>
2022-06-29 00:23:02 +0300

    Update movie mapper

--- src/main/java/com/eneskacan/moviesapi/mapper/MovieMapper.java
+++ src/main/java/com/eneskacan/moviesapi/mapper/MovieMapper.java
@@ -1,6 +1,8 @@
 package com.eneskacan.moviesapi.mapper;
 
 import com.eneskacan.moviesapi.model.Movie;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public final class MovieMapper {
 
@@ -21,8 +23,19 @@ public final class MovieMapper {
                 .build();
     }
 
+    // Transforms JSON object to Movie object
+    public static Movie jsonToMovie(JSONObject json) throws JSONException {
+        return Movie.builder()
+                .imdbId(json.getString("imdbID"))
+                .title(json.getString("Title"))
+                .year(Integer.parseInt(json.getString("Year")))
+                .type(json.getString("Type"))
+                .poster(json.getString("Poster"))
+                .build();
+    }
+
     // Transforms Movie objects to coma separated values
     public static String movieToCsv(Movie m) {
-        return String.format("%s,%s,%s,%s,%s", m.getImdbId(), m.getTitle(), m.getYear(), m.getType(), m.getPoster());
+        return String.format("%s,%s,%s,%s,%s%n", m.getImdbId(), m.getTitle(), m.getYear(), m.getType(), m.getPoster());
     }
 }
